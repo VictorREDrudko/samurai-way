c4fecc72988a4508656aef0d1ef5cf1d2095a972
Victor RED <viktor_RED_rudko@mail.ru>
2024-08-13 11:16:27 +0300

    feat: create dialogs

--- src/components/dialogs/Dialogs.tsx
+++ src/components/dialogs/Dialogs.tsx
@@ -1,5 +1,28 @@
+import { Post } from "../main/posts/post/Post";
+import s from "./Dialogs.module.css"
 export const Dialogs = () => {
+  const dialogsItems = ["Alex", "Addrue", "Fill", "Kane", "Statham Rouny", "Gleb", "Sacha", "Roony"];
+  const mappedDialogsItems = dialogsItems.map((el, index) => {
+    return index === 0 ? <li className={s.item + ' ' + s.activeDialog} key={index}>{el}</li> : <li className={s.item} key={index}>{el}</li>;
+  });
+
   return (
-    <div>DIALOGS</div>
+    <div>
+      <h2>DIALOGS</h2>
+      <div className={s.dialogs}>
+        <div>
+          <ul className={s.listItems}>
+            {mappedDialogsItems}
+          </ul>
+        </div>
+        <div>
+          <Post message="Hi! How are you?" count={9}/>
+        </div>
+        <div>
+
+        </div>
+      </div>
+    </div>
+
   )
 }
